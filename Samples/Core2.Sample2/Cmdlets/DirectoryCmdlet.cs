99680dbc5564246dca29670323d91ccea3a257d7
Daniel Molina <damolina@outlook.com>
2018-10-10 20:24:25 -0700

    Fix: missing argument for Sample2's "dir" was being processed incorrectly.

--- Samples/Core2.Sample2/Cmdlets/DirectoryCmdlet.cs
+++ Samples/Core2.Sample2/Cmdlets/DirectoryCmdlet.cs
@@ -1,5 +1,6 @@
 ﻿using Core2.Commands.Prompt;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
@@ -12,14 +13,21 @@ namespace Core2.Sample2.Cmdlets
     {
         public override Task<PromptCmdletResult> ExecuteAsync(PromptCmdletContext context, params string[] args)
         {
-            var paths = args.Any()
-                ? args.SelectMany(arg => Directory.Exists(arg) ? Directory.EnumerateFileSystemEntries(arg) : new[] { arg })
-                : new[] { Environment.CurrentDirectory };
+            IEnumerable<string> paths;
+
+            if (args.Any())
+            {
+                paths = args.SelectMany(arg => Directory.Exists(arg) ? Directory.EnumerateFileSystemEntries(arg) : new[] { arg });
+            }
+            else
+            {
+                paths = Directory.EnumerateFileSystemEntries(Environment.CurrentDirectory);
+            }
 
             foreach (var path in paths)
             {
                 var fileInfo = new FileInfo(path);
-                Console.WriteLine(fileInfo.Name);
+                Console.WriteLine(fileInfo.Exists ? fileInfo.Name : "File Not Found");
             }
 
             return Task.FromResult(PromptCmdletResult.Empty);
